420db1cdd61686841627689387a6dab89ffe1ab4
Hugo Fok <hugo.fok94@gmail.com>
2019-06-02 18:34:26 -0400

    [Init}

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import Header from '../components/Header';
+const Index = () => (
+  <div>
+    {/* <Link href="/about">
+      <a title="About Page">About Page</a> 
+      <button>About Page</button>
+    </Link> */}
+    <Header />
+    <p>Hello Next.js</p>
+  </div>
+);
+
+export default Index;
